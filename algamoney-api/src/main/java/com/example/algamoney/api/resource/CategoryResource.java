7c0c4ee5b24d6c9d29ced59d1bb369f2dfb5fc9d
qwa-rwansouza <rwan.souza@qwasolucoes.com.br>
2021-11-18 11:18:32 -0300

    Caterory create and getById

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoryResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoryResource.java
@@ -1,11 +1,22 @@
 package com.example.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.model.Category;
 import com.example.algamoney.api.repository.CategoryRepository;
@@ -20,5 +31,22 @@ public class CategoryResource {
 	@GetMapping
 	public List<Category> findAll() {
 		return categoryRepository.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
+		Category categorySave = categoryRepository.save(category);
+		
+		URI uri =  ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(categorySave.getId()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categorySave);
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Category> getById(@PathVariable Long id) {
+		return categoryRepository.findById(id);
 	}
 }
